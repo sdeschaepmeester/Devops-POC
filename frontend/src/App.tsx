300448c5cd20fce8fb481f6481aa7e2a67392e5a
Samantha D <deschaepmeester.samantha@gmail.com>
2024-07-09 19:13:57 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          C'est la partie front end du projet !
+          C'est la partie front end du projet, en React !
         </p>
         <a
           className="App-link"
