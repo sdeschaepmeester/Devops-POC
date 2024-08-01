7a0ab4a51fe7f714569794ef858dec110b6d4ed6
Samantha D <deschaepmeester.samantha@gmail.com>
2024-08-01 16:58:23 +0200

    add connection db

--- backend/src/medhead/Main.java
+++ backend/src/medhead/Main.java
@@ -1,7 +1,44 @@
 package medhead;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Bienvenue !");
-	}
+    public static void main(String[] args) {
+        //String url = System.getenv("DATABASE_URL"); // Assurez-vous que DATABASE_URL est configuré dans votre environnement
+        String url = "postgresql://postgres.ueqjqptqgpomxlznebun:viorne99!-drew@aws-0-eu-central-1.pooler.supabase.com:6543/postgres";
+		String user = "postgres.ueqjqptqgpomxlznebun";
+        String password = "viorne99!-drew";
+                System.out.println("coucou !");
+
+        try {
+			System.out.println("salut !");
+            Class.forName("org.postgresql.Driver");
+            
+            Connection conn = DriverManager.getConnection(url, user, password);
+            
+            Statement stmt = conn.createStatement();
+            
+            String query = "SELECT * FROM users";
+            ResultSet rs = stmt.executeQuery(query);
+            
+            // Afficher les résultats
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String username = rs.getString("username");
+                String createdAt = rs.getString("created_at");
+                
+                System.out.println("ID: " + id + ", Username: " + username + ", Created At: " + createdAt);
+            }
+            
+            // Fermer les ressources
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
