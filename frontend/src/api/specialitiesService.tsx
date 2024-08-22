36764f2833cf3b9826cc3237770eabd5f8cc6a48
Samantha D <deschaepmeester.samantha@gmail.com>
2024-08-22 12:43:22 +0200

    Add auth for api in front

--- frontend/src/api/specialitiesService.tsx
+++ frontend/src/api/specialitiesService.tsx
@@ -2,6 +2,10 @@ import axios from 'axios';
 import SpecialityModel from '../models/specialityModel';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_USERNAME = process.env.REACT_APP_API_USERNAME;
+const API_PASSWORD = process.env.REACT_APP_API_PASSWORD;
+
+const authHeader = 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`);
 
 // Get all specialities
 export const getSpecialities = async (): Promise<SpecialityModel[]> => {
@@ -9,11 +13,12 @@ export const getSpecialities = async (): Promise<SpecialityModel[]> => {
         const response = await axios.get<SpecialityModel[]>(`${BASE_URL}/Specialities`, {
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
+                'Authorization': authHeader,
             }
         });
         return response.data;
     } catch (error) {
-        throw 'Failed to fetch specialities: ' + error;
+        console.error('Failed to fetch specialities:', error);
+        throw new Error('Failed to fetch specialities: ' + error);
     }
 };
